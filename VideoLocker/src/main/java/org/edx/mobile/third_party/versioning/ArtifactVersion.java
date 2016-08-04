fd152e7cff92c77158ad4e4dfc6dd0d9cac898fc
Ibraheem Zaman <ibraheem@edx.org>
2016-08-04 10:57:17 +0500

    Remove NOTICE file refs from ASF license headers

--- VideoLocker/src/main/java/org/edx/mobile/third_party/versioning/ArtifactVersion.java
+++ VideoLocker/src/main/java/org/edx/mobile/third_party/versioning/ArtifactVersion.java
@@ -4,9 +4,8 @@ package org.edx.mobile.third_party.versioning;
  * This class has been copied from the Maven Artifact project.
  *
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * or more contributor license agreements. The ASF licenses
+ * this file to you under the Apache License, Version 2.0 (the
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
