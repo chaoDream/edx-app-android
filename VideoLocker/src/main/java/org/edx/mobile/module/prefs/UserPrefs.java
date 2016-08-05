7d2832ed1403d9e36bb3671b1863af0228ef1549
Mian Khalid <mkhalid@edx.org>
2016-08-05 19:02:57 +0500

    Migrations and folders restructuring
    - Separate folder for videos made in which SHA1 encrypted username based
    folders are made for each logged in user, storing there videos in it.
    - Subtitles folder renamed and place in the videos folder.
    - Tests added for SHA1 util function.

--- VideoLocker/src/main/java/org/edx/mobile/module/prefs/UserPrefs.java
+++ VideoLocker/src/main/java/org/edx/mobile/module/prefs/UserPrefs.java
@@ -2,7 +2,6 @@ package org.edx.mobile.module.prefs;
 
 
 import android.content.Context;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -12,6 +11,8 @@ import com.google.inject.Singleton;
 import org.edx.mobile.logger.Logger;
 import org.edx.mobile.model.api.ProfileModel;
 import org.edx.mobile.user.ProfileImage;
+import org.edx.mobile.util.AppConstants;
+import org.edx.mobile.util.Sha1Util;
 
 import java.io.File;
 import java.io.IOException;
@@ -51,20 +52,21 @@ public class UserPrefs {
      */
     public File getDownloadFolder() {
         ProfileModel profile = getProfile();
-        
-        File android = new File(Environment.getExternalStorageDirectory(), "Android");
-        File downloadsDir = new File(android, "data");
-        File packDir = new File(downloadsDir, context.getPackageName());
-        File edxDir = new File(packDir, profile.username);
-        edxDir.mkdirs();
+        File videosDir = new File(context.getExternalFilesDir(null).getParent(), AppConstants.Folders.VIDEOS);
+        File usersVidDir = new File(videosDir, Sha1Util.SHA1(profile.username));
+        if (!usersVidDir.exists()) {
+            usersVidDir.mkdirs();
+        }
         try {
-            File noMediaFile = new File(edxDir, ".nomedia");
-            noMediaFile.createNewFile();
-        }catch (IOException ioException){
+            File noMediaFile = new File(usersVidDir, ".nomedia");
+            if (!noMediaFile.exists()) {
+                noMediaFile.createNewFile();
+            }
+        } catch (IOException ioException) {
             logger.error(ioException);
         }
-        
-        return edxDir;
+
+        return usersVidDir;
     }
 
     @Nullable
--- VideoLocker/src/main/java/org/edx/mobile/util/AppConstants.java
+++ VideoLocker/src/main/java/org/edx/mobile/util/AppConstants.java
@@ -1,7 +1,7 @@
 package org.edx.mobile.util;
 
 
-public class AppConstants {
+public final class AppConstants {
     
     public static boolean myVideosDeleteMode  = false;
     public static boolean videoListDeleteMode  = false;
@@ -9,4 +9,9 @@ public class AppConstants {
     public static final String VIDEOLIST_BACK_PRESSED = "offline_video_back_pressed";
 
     public static final double MILLISECONDS_PER_SECOND = 1000.00;
+
+    public static final class Folders {
+        public static final String VIDEOS = "videos";
+        public static final String SUBTITLES = "subtitles";
+    }
 }
